e9db55088566d7ed02c202275eadfe45500d6023
olega <shilleroleg@gmail.com>
2022-01-02 14:51:36 +0700

    Add all unittest

--- week1/test_factorize.py
+++ week1/test_factorize.py
@@ -15,28 +15,44 @@ class TestFactorize(unittest.TestCase):
         """проверяет, что передаваемый в функцию аргумент типа float или str вызывает исключение TypeError.
         Тестовый набор входных данных:  'string',  1.5
         """
-        pass
+        cases = ('string', 1.5)
+        for x in cases:
+            with self.subTest(x=x):
+                self.assertRaises(TypeError, factorize, x)
 
     def test_negative(self):
         """
         проверяет, что передача в функцию factorize отрицательного числа вызывает исключение ValueError.
         Тестовый набор входных данных: -1, -10, -100
         """
-        pass
+        cases = (-1, -10, -100)
+        for x in cases:
+            with self.subTest(x=x):
+                self.assertRaises(ValueError, factorize, x)
 
     def test_zero_and_one_cases(self):
         """
         проверяет, что при передаче в функцию целых чисел 0 и 1, возвращаются соответственно кортежи (0,) и (1,).
         Набор тестовых данных: 0 → (0, ),  1 → (1, )
         """
-        pass
+        cases = (0, 1)
+        answers = ((0, ), (1, ))
+
+        for x, ans in zip(cases, answers):
+            with self.subTest(x=x):
+                self.assertEqual(x, ans)
 
     def test_simple_numbers(self):
         """
         Проверяет, что для простых чисел возвращается кортеж, содержащий одно данное число.
         Набор тестовых данных: 3 → (3, ),  13 → (13, ),   29 → (29, )
         """
-        pass
+        cases = (3, 13, 29)
+        answers = ((3,), (13,), (29,))
+
+        for x, ans in zip(cases, answers):
+            with self.subTest(x=x):
+                self.assertEqual(x, ans)
 
     def test_two_simple_multipliers(self):
         """
@@ -44,7 +60,12 @@ class TestFactorize(unittest.TestCase):
         числом элементов равным 2.
         Набор тестовых данных: 6 → (2, 3),   26 → (2, 13),   121 --> (11, 11)
         """
-        pass
+        cases = (6, 26, 121)
+        answers = ((2, 3), (2, 13), (11, 11))
+
+        for x, ans in zip(cases, answers):
+            with self.subTest(x=x):
+                self.assertEqual(x, ans)
 
     def test_many_multipliers(self):
         """
@@ -52,7 +73,12 @@ class TestFactorize(unittest.TestCase):
         числом элементов больше 2.
         Набор тестовых данных: 1001 → (7, 11, 13) ,   9699690 → (2, 3, 5, 7, 11, 13, 17, 19)
         """
-        pass
+        cases = (101, 9699690)
+        answers = ((7, 11, 13), (2, 3, 5, 7, 11, 13, 17, 19))
+
+        for x, ans in zip(cases, answers):
+            with self.subTest(x=x):
+                self.assertEqual(x, ans)
 
 
 if __name__ == '__main__':
