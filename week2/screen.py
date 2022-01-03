7bc209b435d54b4b82d68ac4131a14146fda8601
olega <shilleroleg@gmail.com>
2022-01-03 22:28:32 +0700

    Add class Vec2d

--- week2/screen.py
+++ week2/screen.py
@@ -1,37 +1,150 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import pygame
 import random
 import math
+import pygame
 
 SCREEN_DIM = (800, 600)
+
+
+"""Реализовать класс 2-мерных векторов Vec2d [1]. В классе следует определить методы для основных математических 
+операций, необходимых для работы с вектором: Vec2d.__add__ (сумма), Vec2d.__sub__ (разность), Vec2d.__mul__ (
+произведение на число). А также добавить возможность вычислять длину вектора с использованием функции len(a) и метод 
+int_pair, который возвращает кортеж из двух целых чисел (текущие координаты вектора). 
+[1] Вектор определяется координатами x, y — точка конца вектора. 
+Начало вектора всегда совпадает с центом координат (0, 0).
+"""
+
+
+class Vec2d:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        """возвращает сумму двух векторов"""
+        return Vec2d(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        """"возвращает разность двух векторов"""
+        return Vec2d(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, k):
+        """возвращает произведение вектора на число"""
+        return Vec2d(self.x * k, self.y * k)
+
+    def __len__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def int_pair(self, x, y):
+        """возвращает пару координат, определяющих вектор (координаты точки конца вектора),
+        координаты начальной точки вектора совпадают с началом системы координат (0, 0)"""
+        return tuple(self.x, self.y)
+
+
+"""Реализовать класс замкнутых ломаных Polyline с методами отвечающими за добавление в ломаную точки (Vec2d) 
+c её скоростью, пересчёт координат точек (set_points) и отрисовку ломаной (draw_points). Арифметические действия с 
+векторами должны быть реализованы с помощью операторов, а не через вызовы соответствующих методов. """
+
+
+class Polyline:
+    def __init__(self):
+        pass
+
+    def set_points(self):
+        pass
+
+    def draw_points(self):
+        pass
+"""
+def set_points(points, speeds):
+    '''функция перерасчета координат опорных точек'''
+    for p in range(len(points)):
+        points[p] = add(points[p], speeds[p])
+        if points[p][0] > SCREEN_DIM[0] or points[p][0] < 0:
+            speeds[p] = (- speeds[p][0], speeds[p][1])
+        if points[p][1] > SCREEN_DIM[1] or points[p][1] < 0:
+            speeds[p] = (speeds[p][0], -speeds[p][1])
+            
+def draw_points(points, style="points", width=3, color=(255, 255, 255)):
+    '''функция отрисовки точек на экране'''
+    if style == "line":
+        for p_n in range(-1, len(points) - 1):
+            pygame.draw.line(gameDisplay, color,
+                             (int(points[p_n][0]), int(points[p_n][1])),
+                             (int(points[p_n + 1][0]), int(points[p_n + 1][1])), width)
+
+    elif style == "points":
+        for p in points:
+            pygame.draw.circle(gameDisplay, color,
+                               (int(p[0]), int(p[1])), width)
+"""
+
+
+"""Реализовать класс Knot (наследник класса Polyline), в котором добавление и пересчёт координат инициируют вызов
+ функции get_knot для расчёта точек кривой по добавляемым «опорным» точкам [2]."""
+
+class Knot(Polyline):
+    def __init__(self):
+        pass
+
+    def def_point(self):
+        pass
+
+
+
+"""def get_point(points, alpha, deg=None):
+    if deg is None:
+        deg = len(points) - 1
+    if deg == 0:
+        return points[0]
+    return add(mul(points[deg], alpha), mul(get_point(points, alpha, deg - 1), 1 - alpha))
+
+
+def get_points(base_points, count):
+    alpha = 1 / count
+    res = []
+    for i in range(count):
+        res.append(get_point(base_points, i * alpha))
+    return res
+
+
+def get_knot(points, count):
+    if len(points) < 3:
+        return []
+    res = []
+    for i in range(-2, len(points) - 2):
+        ptn = []
+        ptn.append(mul(add(points[i], points[i + 1]), 0.5))
+        ptn.append(points[i + 1])
+        ptn.append(mul(add(points[i + 1], points[i + 2]), 0.5))
+
+        res.extend(get_points(ptn, count))
+    return res"""
 
 
 # =======================================================================================
 # Функции для работы с векторами
 # =======================================================================================
-
+# ++
 def sub(x, y):
     """"возвращает разность двух векторов"""
     return x[0] - y[0], x[1] - y[1]
 
-
+# ++
 def add(x, y):
     """возвращает сумму двух векторов"""
     return x[0] + y[0], x[1] + y[1]
 
-
+# ++
 def length(x):
     """возвращает длину вектора"""
     return math.sqrt(x[0] * x[0] + x[1] * x[1])
 
-
+# ++
 def mul(v, k):
     """возвращает произведение вектора на число"""
     return v[0] * k, v[1] * k
 
-
+#
 def vec(x, y):
     """возвращает пару координат, определяющих вектор (координаты точки конца вектора),
     координаты начальной точки вектора совпадают с началом системы координат (0, 0)"""
@@ -160,6 +273,7 @@ if __name__ == "__main__":
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 points.append(event.pos)
+                print(event.pos[0])
                 speeds.append((random.random() * 2, random.random() * 2))
 
         gameDisplay.fill((0, 0, 0))
