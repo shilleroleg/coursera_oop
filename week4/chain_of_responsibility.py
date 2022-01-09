d5ed1fe920c23d7c539065ef01280c1fb47e8908
olega <shilleroleg@gmail.com>
2022-01-09 18:57:38 +0700

    add comments - pattern chain of responsibility

--- week4/chain_of_responsibility.py
+++ week4/chain_of_responsibility.py
@@ -1,3 +1,23 @@
+"""
+Реализовать Chain of Responsibility.
+Необходимо реализовать поведение:
+    EventGet(<type>) создаёт событие получения данных соответствующего типа
+    EventSet(<value>) создаёт событие изменения поля типа type(<value>)
+
+Необходимо реализовать классы NullHandler, IntHandler, FloatHandler, StrHandler так, чтобы можно было создать цепочку:
+chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
+
+Описание работы цепочки:
+
+    chain.handle(obj, EventGet(int)) — вернуть значение obj.integer_field
+    chain.handle(obj, EventGet(str)) — вернуть значение obj.string_field
+    chain.handle(obj, EventGet(float)) — вернуть значение obj.float_field
+    chain.handle(obj, EventSet(1)) — установить значение obj.integer_field =1
+    chain.handle(obj, EventSet(1.1)) — установить значение obj.float_field = 1.1
+    chain.handle(obj, EventSet("str")) — установить значение obj.string_field = "str"
+"""
+
+
 class SomeObject:
     def __init__(self):
         self.integer_field = 0
